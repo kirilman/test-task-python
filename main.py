332b8951388921cd574a6fefefa1ab26179e15ff
kirilman03 <kirilman03@gmail.com>
2020-11-20 16:47:52 +0700

    advanced result

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
-
-import csv, json
-
-from parser import get_n, get_columns_names, preprocessing_csv_one, preprocessing_csv_two, preprocessing_json_file, preprocessing_xml_file
+import csv, json, sys
+import argparse
+from parser import get_n, get_columns_names, preprocessing_csv_one, preprocessing_csv_two
+from parser import preprocessing_json_file, preprocessing_xml_file, sort_resulting_file, group_result_file
 # from parser import get_n, get_columns_names
 
-file = open('csv_data_1.csv','r')
-reader = csv.DictReader(file)
-n = get_n(reader)
-fields = get_columns_names(reader)
+
+def preprocessing_files(file_name_one,
+                        file_name_two,
+                        file_name_json,
+                        file_name_xml,
+                        file_name_result):
+
+    file_one = open(file_name_one,'r')
+    reader = csv.DictReader(file_one)
+    n = get_n(reader)
+    fields = get_columns_names(reader)
+
+    out_f = open('test_out.csv','w')
+    writer = csv.DictWriter(out_f, fieldnames=fields, delimiter=',')
+    writer.writeheader()
+
+    preprocessing_csv_one(reader, n, fields, writer)
+    file_one.close()
+
+    try:
+        file_two = open(file_name_two,'r')
+        reader = csv.DictReader(file_two)
+        preprocessing_csv_two(reader, n, fields, writer)
+        file_two.close()
+    except Exception as exc:
+        print('Failed to process the {} file, error: {}'.format(file_name_two, exc))
+        out_f.close()
+        return
+
+    try:
+        file_json = open(file_name_json,'r')
+        data = json.load(file_json)
+        file_json.close()
+        preprocessing_json_file(data, fields, writer)
+    except Exception as exc:
+        print('Failed to process the {} file, error: {}'.format(file_name_json, exc))
+        out_f.close()
+        return
+    try:
+        preprocessing_xml_file(file_name_xml, fields, writer)
+    except Exception as exc:
+        print('Failed to process the {} file, error: {}'.format(file_name_xml, exc))
+        out_f.close()
+        return
+    out_f.close()
+
+    try:
+        sort_resulting_file('test_out.csv', file_name_result)
+    except Exception as exc:
+        print('Failed to sort result file, error: {}'.format(exc))
 
 
-out_f = open('test_out.csv','w')
-writer = csv.DictWriter(out_f, fieldnames=fields, delimiter=',')
-writer.writeheader()
-
-file = open('csv_data_1.csv','r')
-reader = csv.DictReader(file)
-preprocessing_csv_one(reader, n, fields, writer)
-file.close()
-
-file = open('csv_data_2.csv','r')
-reader = csv.DictReader(file)
-preprocessing_csv_two(reader, n, fields, writer)
-file.close()
-
-file = open('json_data.json','r')
-data = json.load(file)
-file.close()
-
-preprocessing_json_file(data, fields, writer)
-preprocessing_xml_file('xml_data.xml', fields, writer)
+    group_result_file(file_name_result,'adv_results.tsv', n)
 
 
-out_f.close()
+preprocessing_files('csv_data_1.csv','csv_data_2.csv', 'json_data.json', 'xml_data.xml', 'result_test.tsv')
--- parser.py
+++ parser.py
@@ -1,18 +1,22 @@
 import csv, json
 from xml.etree import ElementTree
+import os
 
 def get_n(file_reader):
     l = list(filter(lambda x: x[0] == 'D', file_reader.fieldnames))
     return len(l)
 
 def get_columns_names(file_reader):
+    """
+        Получить имена столбцов из первого csv файла
+    """
     field = file_reader.fieldnames
     d_fields = list(filter(lambda x: x[0] == 'D', file_reader.fieldnames))
     m_fields = list(filter(lambda x: x[0] == 'M', file_reader.fieldnames))
     
     if len(d_fields) != len(m_fields):
         print('Dimensions of D,M are not equal')
-        return d_fields + m_fields[:-1]
+        return d_fields + m_fields[:len(d_fields)]
     return d_fields + m_fields
 
 def preprocessing_csv_one(file_reader, n, fields, writer):
@@ -38,3 +42,70 @@ def preprocessing_xml_file(file_name, fields, writer):
     for obj in objects:
         insert_row = {item.attrib['name']: item.find('value').text for item in obj}
         writer.writerow(insert_row)
+
+def sort_resulting_file(inpt_file_name, out_file_name):
+    """
+        Сортировка файла по первому столбцу
+    """
+    file = open(inpt_file_name, 'r')
+    lines = file.readlines()
+    head = lines[0]
+    lines = lines[1:]
+    file.close()
+    
+    first_row = []
+    for line in lines:
+        s = line.split(',')[0]
+        first_row.append(s)   
+    indx = [e[0] for e in sorted(enumerate(first_row), key = lambda x:x[1])]
+    
+    #перекладываем в новый список
+    new_list = [None for i in range(len(first_row))]
+    for i, s in enumerate(indx):
+        new_list[i] = lines[s]
+        
+    file = open(out_file_name, 'w')
+    file.write(head.replace(',','\t'))
+    for line in new_list:
+        file.write(line.replace(',','\t'))
+    file.close()    
+    os.remove(inpt_file_name)
+
+def group_result_file(result_filename, result_group_filename, n):
+    """
+        Сумма значений по сгруппированным комбинациям
+        Advanced_result
+    """
+    file = open(result_filename,'r')
+    lines = file.readlines()
+    head = lines[0].replace('\n','').split('\t')
+    lines = lines[1:]
+    file.close()
+    
+    def summa_lines(pred_line, line, n):
+        new_line = pred_line[:n]
+        elems = [int(a)+int(b) for a,b in zip(pred_line[n:], line[n:])]
+        return new_line + elems
+    
+    #Формируем список со суммой значений сгруппированой по комбинации строк
+    new_lines = []
+    for k, line in enumerate(lines):
+        elems = line.replace('\n','').split('\t')
+        if k == 0:
+            pred_elems = elems
+        if pred_elems[:n] == elems[:n]:
+            if k == 0:
+                new_lines.append(elems)
+                continue
+            new_lines[-1] = summa_lines(pred_elems, elems, n)  
+        else:
+            new_lines.append(elems)
+        pred_elems = elems
+        
+    #Запись сформированного списка в файл
+    file = open(result_group_filename,'w')
+    writer = csv.writer(file, delimiter='\t')
+    writer.writerow(head)
+    for line in new_lines:
+        writer.writerow(line)
+    file.close()
